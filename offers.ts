372d6ea57adb445d83651d662876a67a1dc956c0
hyein89 <bihontai@gmail.com>
2025-07-31 02:36:11 +0700

    Update offers.ts

--- offers.ts
+++ offers.ts
@@ -1,8 +1,10 @@
 // offers.ts
 const offerMap: Record<string, string> = {
-  fr: "https://example.com/fr-offer=",
-  id: "https://example.com/id-offer=",
-  us: "https://example.com/us-offer=",
+  fr: "https://smrturl.co/a/s55095c62fb/13344?s1=",
+  pl: "https://smrturl.co/a/s55095c62fb/4166?s1=",
+  us: "https://smrturl.co/a/s55095c62fb/12965?s1=",
+  dk: "https://smrturl.co/a/s55095c62fb/13880?s1=",
+  mx: "https://smrturl.co/a/s55095c62fb/13878?s1=",
   default: "https://grubpaths.com/q3sc09250?key=d2f645145443ac96251458e6cadaac1b"
 };
 
