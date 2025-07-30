30ce92f72d52f29e30d1344ad629085b73afec86
hyein89 <bihontai@gmail.com>
2025-07-30 23:42:23 +0700

    Update offers.ts

--- offers.ts
+++ offers.ts
@@ -1,8 +1,8 @@
 // offers.ts
 const offerMap: Record<string, string> = {
-  fr: "https://example.com/fr-offer",
-  id: "https://example.com/id-offer",
-  us: "https://example.com/us-offer",
+  fr: "https://example.com/fr-offer=",
+  id: "https://example.com/id-offer=",
+  us: "https://example.com/us-offer=",
   default: "https://example.com/global-offer"
 };
 
