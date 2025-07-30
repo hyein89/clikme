8e2df0bda2332ee598cc102cd683b9a19e6427b1
hyein89 <bihontai@gmail.com>
2025-07-31 06:49:49 +0700

    Update offers.ts

--- offers.ts
+++ offers.ts
@@ -2,7 +2,7 @@
 const offerMap: Record<string, string> = {
   fr: "https://smrturl.co/a/s55095c62fb/13344?s1=",
   pl: "https://smrturl.co/a/s55095c62fb/4166?s1=",
-  us: "https://smrturl.co/a/s55095c62fb/12965?s1=",
+  us: "https://wedebeek.com/click?pid=3574&offer_id=18909&sub1=",
   dk: "https://smrturl.co/a/s55095c62fb/13880?s1=",
   mx: "https://smrturl.co/a/s55095c62fb/13878?s1=",
   default: "https://grubpaths.com/q3sc09250?key=d2f645145443ac96251458e6cadaac1b"
