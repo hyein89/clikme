ccaff0ac317206734dea4426d50dc4a5aac1366c
hyein89 <bihontai@gmail.com>
2025-07-31 01:04:14 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
-// pages/_app.tsx
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Movie & TV Search</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
 
