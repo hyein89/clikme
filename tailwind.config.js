03d7d2a24984ff65ecac7104ba27c0b7f37a5d00
hyein89 <bihontai@gmail.com>
2025-07-31 01:06:23 +0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class",
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/line-clamp")],
-};
